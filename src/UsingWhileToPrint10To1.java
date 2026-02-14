5f00ff8bd93229bbf4c087b39312e65354159d0d
Sriram Abhishek <sriramabhishek1420@gmail.com>
2026-02-14 22:46:05 +0530

    Print Numbers using while from 10 to 1

---
+++ src/UsingWhileToPrint10To1.java
@@ -0,0 +1,10 @@
+public class UsingWhileToPrint10To1 {
+    public static  void main(String[] args){
+        // Using while condition//
+        int  i = 10;
+        while( i >= 1){
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
