470d0de606c9b75b2b4df683663a0bd983ad6d94
Sriram Abhishek <sriramabhishek1420@gmail.com>
2026-02-14 23:16:40 +0530

    Check the even numbers from 1 to 20

---
+++ src/DisplayEvenNumbersFrom1To20.java
@@ -0,0 +1,20 @@
+public class DisplayEvenNumbersFrom1To20 {
+
+    public static void main(String[] args){
+
+                    // ( Solution 1)
+            //        int i ;
+            //        for(i = 0; i <= 0 ;){
+            //            i = i + 2;
+            //            System.out.print(i + " ");
+            //        }
+        System.out.println("/ *****************/");
+        int i ;
+        for(i = 1; i <=20; i++ ){
+            if(i%2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
