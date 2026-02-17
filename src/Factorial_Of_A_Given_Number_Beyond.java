a55eefb4cb9bb38061cdd314285a65621af27f7b
Sriram Abhishek <sriramabhishek1420@gmail.com>
2026-02-18 02:42:47 +0530

    checking input to continue the process

--- src/Factorial_Of_A_Given_Number_Beyond.java
+++ src/Factorial_Of_A_Given_Number_Beyond.java
@@ -5,7 +5,11 @@ public class Factorial_Of_A_Given_Number_Beyond {
     public static void main(String[] args){
        Scanner scan = new Scanner(System.in);
         System.out.print("Enter the number : ");
+        if (!scan.hasNextInt()){
+            System.out.println("Please enter a valid integer.");
+        }
         int number = scan.nextInt();
+
         BigInteger fact = BigInteger.ONE;
         if(number < 0){
             System.out.println("factorial is not defined as a negative number");
