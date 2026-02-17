003a5454a682d2af5b0511554a425669a6002bed
Sriram Abhishek <sriramabhishek1420@gmail.com>
2026-02-18 02:16:55 +0530

    Using BIGINTEGER class in Math package - "Factorial"

---
+++ src/Factorial_Of_A_Given_Number_Beyond.java
@@ -0,0 +1,20 @@
+import java.math.*;
+import java.util.*;
+public class Factorial_Of_A_Given_Number_Beyond {
+
+    public static void main(String[] args){
+       Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int number = scan.nextInt();
+        BigInteger fact = BigInteger.ONE;
+        if(number < 0){
+            System.out.println("factorial is not defined as a negative number");
+        }
+        else{
+            for(int i = 2; i <= number; i++){
+               fact = fact.multiply(BigInteger.valueOf(i));
+            }
+            System.out.println("Factorial of a given number : "+fact);
+        }
+    }
+}
